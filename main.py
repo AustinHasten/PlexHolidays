99860522da04d399e622b42d311ca0a956f766f4
Saul Femm <saulfemm@outlook.com>
2017-11-17 09:15:16 -0600

    Use Plex helper functions for server/section selection

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # Saul Femm
 # Initial Commit - November 16th, 2017
 
-import getpass, urllib.request
+import getpass, urllib.request, sys
 from bs4 import BeautifulSoup
 from plexapi.myplex import MyPlexAccount
 from plexapi.playlist import Playlist
 from plexapi.exceptions import BadRequest, NotFound
+import plexapi.utils
 
 class Plex():
     def __init__(self):
@@ -26,7 +27,7 @@ class Plex():
             try:
                 account = MyPlexAccount(username, password)
             except BadRequest:
-                print('INVALID USERNAME/PASSWORD')
+                print('Invalid Username/Password.')
                 continue
             print('Done')
             break
@@ -41,48 +42,19 @@ class Plex():
             print('No available servers.')
             sys.exit()
 
-        # Loop until valid server name is given
-        while True:
-            print('Available servers:')
-            for x in servers:
-                print('\t', x.name)
-
-            server_name = input('Select server: ')
-            print('Connecting to server... ', end='', flush=True)
-            try:
-                server = account.resource(server_name).connect()
-            except NotFound:
-                print('INVALID SERVER NAME')
-                continue
-            print('Done')
-            break
-
-        return server
+        return plexapi.utils.choose('Select server index', servers, "name").connect()
 
     def get_server_section(self, server):
         """
             Select section from Plex server.
         """
-        sections = [ _ for _ in server.library.sections() if _.type == 'show' ]
+#        sections = [ _ for _ in server.library.sections() if _.type == 'show' ]
+        sections = [ _ for _ in server.library.sections() ]
         if not sections:
             print('No available sections.')
             sys.exit()
 
-        # Loop until valid section name is given
-        while True:
-            print('Available sections:')
-            for section in sections:
-                print('\t', section.title)
-            section = input('Select section: ')
-            print('Getting media from section... ', end='', flush=True)
-            try:
-                plex_section = server.library.section(section)
-            except NotFound:
-                print('INVALID SECTION NAME')
-                continue
-            print('Done')
-            break
-        return plex_section
+        return plexapi.utils.choose('Select section index', sections, 'title')
 
     # TODO Change this function such that it's no longer specific to TV episodes
     def get_matching_media(self, lookup):
@@ -109,14 +81,13 @@ def imdb_search(keyword):
     url = ('http://www.imdb.com/search/title?&title_type=tv_episode&view=simple&count=2000&keywords=' + keyword)
 
     print('Fetching IMDb results... ', end='', flush=True)
-    # Loop over results 100 at a time rather than all at once to prevent timeout
-    html = urllib.request.urlopen(url, timeout=60).read()
+    html = urllib.request.urlopen(url, timeout=180).read()
     soup = BeautifulSoup(html, 'html.parser')
     spans = soup.find_all('span', title=True)
     for span in spans:
         a = span.find_all('a')
 
-        # Show without episode name
+        # Ignore the rare IMDb episodes that have no title
         if len(a) < 2:
             continue
 
@@ -137,10 +108,7 @@ if __name__ == "__main__":
 
     print('Matching Media: ')
     for media in matching_media:
-        season = media.parentIndex.zfill(2)
-        episode = str(media.index).zfill(2)
-        se = ('S' + season + 'E' + episode)
-        print('\t', media.grandparentTitle, '-', se, '-', media.title)
+        print('\t', media.grandparentTitle, '-', media.seasonEpisode.upper(), '-', media.title)
     plex.create_playlist(input('Playlist name: '), matching_media)
 
     print('Happy Holidays!')
