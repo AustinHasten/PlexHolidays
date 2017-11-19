15703264b93cda331ae72cdd4aceffd14930188c
Saul Femm <saulfemm@outlook.com>
2017-11-18 19:49:00 -0600

    Major rewrite. Now supports movies.

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 # Saul Femm
 # Initial Commit - November 16th, 2017
 
-import getpass, urllib.request, sys
-from bs4 import BeautifulSoup
+import sys
+import getpass
+import plexapi.utils
+from tqdm import tqdm
+from imdb import IMDb
 from plexapi.myplex import MyPlexAccount
 from plexapi.playlist import Playlist
-from plexapi.exceptions import BadRequest, NotFound
-import plexapi.utils
+from plexapi.exceptions import BadRequest
 
 class Plex():
     def __init__(self):
         self.account = self.get_account()
         self.server = self.get_account_server(self.account)
         self.section = self.get_server_section(self.server)
-        self.media = self.section.all()
+        self.media = self.get_flat_media()
 
     def get_account(self):
         """
@@ -48,67 +50,120 @@ class Plex():
         """
             Select section from Plex server.
         """
-        sections = [ _ for _ in server.library.sections() if _.type == 'show' ]
+        sections = [ _ for _ in server.library.sections() if _.type in {'movie', 'show'} ]
         if not sections:
             print('No available sections.')
             sys.exit()
 
         return plexapi.utils.choose('Select section index', sections, 'title')
 
-    # TODO Change this function such that it's no longer specific to TV episodes
-    def get_matching_media(self, lookup):
+    def get_flat_media(self):
         """
-            Find matching items between a Plex library section and a dictionary.
+            Flatten this object's section's media list."
         """
-        matching_media = []
-        matching_shows = [ show for show in self.media if show.title in lookup ]
-        for show in matching_shows:
-            for episode in show.episodes():
-                if episode.title.lower() in lookup[show.title]:
-                    matching_media.append(episode)
-
-        return matching_media
+        # Movie sections are already flat
+        if self.section.type == 'movie':
+            return self.section.all()
+        # TV sections are not
+        else:
+            flattened = []
+            for show in self.section.all():
+                for episode in show.episodes():
+                    flattened.append(episode)
+            return flattened
 
     def create_playlist(self, name, media):
-        Playlist.create(self.server, name, media)
-
-def imdb_search(keyword):
-    """
-        Search IMDb for a keyword and parse results into a dictionary.
-    """
-    results = dict()
-    keyword = keyword.lower().replace(' ', '-')
-    url = ('http://www.imdb.com/search/title?&title_type=tv_episode&view=simple&count=2000&keywords=' + keyword)
-
-    print('Fetching IMDb results... ', end='', flush=True)
-    html = urllib.request.urlopen(url, timeout=180).read()
-    soup = BeautifulSoup(html, 'html.parser')
-    spans = soup.find_all('span', title=True)
-    for span in spans:
-        a = span.find_all('a')
-
-        # Ignore the rare IMDb episodes that have no title
-        if len(a) < 2:
-            continue
-
-        show_name = a[0].contents[0].strip()
-        episode_name = a[1].contents[0].strip().lower()
-        if show_name in results:
-            results[show_name].append(episode_name)
+        """
+            Create or update playlist with list of media.
+        """
+        for playlist in self.server.playlists():
+            if name == playlist.title:
+                playlist.addItems(media)
+                break
         else:
-            results[show_name] = [ episode_name ]
-    print('Done')
+            Playlist.create(self.server, name, media)
 
-    return results
+class PlexHolidays():
+    def __init__(self):
+        self.plex = Plex()
+        self.imdb = IMDb()
+        keyword = input('Keyword (i.e. Holiday name): ')
+        keyword_matches = []
+
+        print('Scanning', self.plex.section.title, '...')
+        for plex_medium in tqdm(self.plex.media):
+            imdb_medium = self.plex2imdb(plex_medium)
+
+            if not imdb_medium:
+                continue
+
+            keywords = self.get_keywords(imdb_medium)
+            if keyword in keywords:
+                keyword_matches.append(plex_medium)
+
+        print('Titles matching\"', keyword, '\" :')
+        for match in keyword_matches:
+            print('\t', match.title)
+        self.plex.create_playlist(input('Playlist name: '), keyword_matches)
+
+        print('Happy Holidays!')
+
+    def plex2imdb(self, medium):
+        """
+            Get the IMDbPy object for a given Plex object.
+        """
+        # Set appropriate search method and acceptable results based on section type
+        if self.plex.section.type == 'movie':
+            kinds = {'movie', 'short', 'tv movie', 'tv short'}
+            search_function = self.imdb.search_movie
+        else:
+            kinds = {'episode'}
+            search_function = self.imdb.search_episode
+
+        # Perform IMDb search for the Plex object
+        while True:
+            try:
+                results = [ _ for _ in search_function(medium.title) if _['kind'] in kinds ]
+                break
+            # Time out, try again.
+            except OSError:
+                print('Timed out while downloading', medium.title)
+                continue
+
+        # No IMDb results whatsoever
+        if not results:
+            return None
+        # Plex has no year listed, return first search result
+        elif not medium.year:
+            return results[0]
+
+        closest_result, closest_year = None, 9999
+        for result in results:
+            # This result has no year listed, ignore it.
+            if not result.get('year'):
+                continue
+
+            # Exact match found
+            if result['year'] == medium.year:
+                return result
+            # Track match with closest year in case exact match is not found
+            elif (medium.year - result['year']) < closest_year:
+                closest_result = result
+        # No exact match found, use result with closest year
+        else:
+            return closest_result
+
+    def get_keywords(self, imdb_obj):
+        """
+            Get the plot keywords for a given IMDbPy object.
+        """
+        if not imdb_obj:
+            return []
+
+        data = self.imdb.get_movie_keywords(imdb_obj.movieID)['data']
+        if not 'keywords' in data:
+            return []
+        return data['keywords']
 
 if __name__ == "__main__":
-    plex = Plex()
-    imdb_results = imdb_search(input('Keyword (i.e. Holiday Name): '))
-    matching_media = plex.get_matching_media(imdb_results)
-
-    print('Matching Media: ')
-    for media in matching_media:
-        print('\t', media.grandparentTitle, '-', media.seasonEpisode.upper(), '-', media.title)
-    plex.create_playlist(input('Playlist name: '), matching_media)
-
-    print('Happy Holidays!')
+    PH = PlexHolidays()
