c9703acad34bc7203ac60bcc485d1e37510eeb96
austinhasten@live.com <austinhasten@live.com>
2019-10-30 18:20:34 -0500

    Switch TVDB packages

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 # Austin Hasten
 # Initial Commit - November 16th, 2017
 import re, sys, logging, itertools
+import tvdb_api
 import plexapi.utils
 from tqdm import tqdm
 from retry import retry
-from pytvdbapi import api
 from multiprocessing.pool import ThreadPool
 from imdb import IMDb, IMDbDataAccessError
 from pytvdbapi.error import TVDBIndexError
@@ -55,7 +55,7 @@ class PlexHolidays():
         logging.getLogger('imdbpy').disabled = True
         logging.getLogger('imdbpy.parser.http.urlopener').disabled = True
         self.imdbpy = IMDb()
-        self.tvdb = api.TVDB('B43FF87DE395DF56')
+        self.tvdb = tvdb_api.Tvdb()
         self.plex = Plex()
         self.keyword = input('Keyword (i.e. Holiday name): ').lower()
         self.pbar = tqdm(self.plex.media, desc=f'{self.plex.section.title}')
@@ -81,7 +81,8 @@ class PlexHolidays():
         finally:
             self.pbar.update()
 
-    @retry(ConnectTimeout, delay=1)
+    # TODO Handle exception if tries exceeded
+    @retry(ConnectTimeout, delay=1, tries=5)
     def get_plex_guid(self, medium):
         return medium.guid
 
@@ -96,22 +97,25 @@ class PlexHolidays():
         regex = r'\/\/(\d*)\/(\d*)\/(\d*)'
         series_id, season, episode = map(int, re.search(regex, plex_guid).groups())
         try:
-            episode = self.get_tvdb_series(series_id)[season][episode]
-        except TVDBIndexError:
+            imdb_id = self.tvdb[series_id][season][episode]['imdbId']
+        except (NameError, tvdb_api.tvdb_episodenotfound, tvdb_api.tvdb_seasonnotfound):
             return None
-        imdb_id = str(episode.IMDB_ID)
         return imdb_id[2:] if imdb_id.startswith('tt') else imdb_id
 
-    @retry((RemoteDisconnected, ResponseNotReady, AttributeError, BrokenPipeError, OSError), delay=1)
+    # TODO Handle exception if tries exceeded
+    @retry((RemoteDisconnected, ResponseNotReady, AttributeError, BrokenPipeError, OSError), delay=1, tries=5)
     def get_tvdb_series(self, series_id):
         return self.tvdb.get_series(series_id, 'en')
 
-    @retry(IMDbDataAccessError, delay=1)
+    # TODO Handle exception if tries exceeded
+    @retry(IMDbDataAccessError, delay=2, tries=5)
     def get_imdb_keywords(self, imdb_id):
         if not imdb_id:
             return []
-        data = self.imdbpy.get_movie_keywords(imdb_id)['data']
-        return data.get('keywords', [])
+        try:
+            return self.imdbpy.get_movie_keywords(imdb_id)['data'].get('keywords', [])
+        except IMDbDataAccessError:
+            return []
 
 if __name__ == "__main__":
     ph = PlexHolidays()
